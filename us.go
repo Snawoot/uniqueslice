7f6cea95119107dc66f8a126c9a911361728d1fd
Vladislav Yarmak <vladislav@vm-0.com>
2025-05-05 23:07:34 +0300

    go doc

--- us.go
+++ us.go
@@ -5,6 +5,9 @@ import (
 	"unique"
 )
 
+// Make returns a globally unique handle for a slice of values of type T. Handles
+// are equal if and only if the values used to produce them are equal.
+// Make is safe for concurrent use by multiple goroutines.
 func Make[Slice ~[]T, T comparable](s Slice) Handle[Slice, T] {
 	l := len(s)
 	arrTyp := reflect.ArrayOf(l, reflect.TypeFor[T]())
@@ -16,10 +19,17 @@ func Make[Slice ~[]T, T comparable](s Slice) Handle[Slice, T] {
 	}
 }
 
+// Handle is a globally unique identity for some slice of value of type T.
+//
+// Two handles compare equal exactly if the two values used to create the handles
+// would have also compared equal. The comparison of two handles is trivial and
+// typically much more efficient than comparing the values used to create them.
 type Handle[Slice ~[]T, T comparable] struct {
 	h unique.Handle[any]
 }
 
+// Value returns a slice of shallow copies of the T value that produced the Handle.
+// Value is safe for concurrent use by multiple goroutines.
 func (h Handle[Slice, T]) Value() Slice {
 	vi := h.h.Value()
 	rv := reflect.ValueOf(vi)
